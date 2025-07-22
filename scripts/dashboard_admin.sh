c850f933537de5729990803b20b838cad6bbfe82
Ayesha <u2104008@student.cuet.ac.bd>
2025-07-23 00:19:55 +0600

    Update dashboard_admin.sh

--- scripts/dashboard_admin.sh
+++ scripts/dashboard_admin.sh
@@ -17,8 +17,8 @@ while true; do
 
         {
             echo "Problem ID, Title, Difficulty, Status"
-            awk -F',' 'tolower($4) ~ /available/' ../problems.txt
-            awk -F',' 'tolower($4) !~ /available/' ../problems.txt
+            awk -F',' 'tolower($4) ~ /available/' problems.txt
+            awk -F',' 'tolower($4) !~ /available/' problems.txt
         } | column -s, -t
         ;;
     3) 
@@ -27,14 +27,14 @@ while true; do
 
         echo "🔍$pid:"
         echo "-----------------------------------"
-        cat "../problems/$pid/statement.txt"
+        cat "problems/$pid/statement.txt"
         echo "-----------------------------------"
         ;;
     4)
         echo "👥 Users:"
         {
           echo "Username, Role"
-          awk -F',' '{gsub(/^ +| +$/, "", $0); print $1 "," $3}' ../users.txt
+          awk -F',' '{gsub(/^ +| +$/, "", $0); print $1 "," $3}' users.txt
         } | column -s, -t
       
        ;;
