9dc7159a83ee989e99f6f706ce5fb1c5eeecc2ed
Ayesha <u2104008@student.cuet.ac.bd>
2025-07-23 00:26:07 +0600

    Update practice_submission.sh

--- scripts/practice_submission.sh
+++ scripts/practice_submission.sh
@@ -9,12 +9,12 @@ read -p "Solution file path: " spath
 
 
 # Save submission
-mkdir -p "../submissions/$cname" #PATH
+mkdir -p "submissions/$cname" #PATH
 ext="${spath##*.}"
-dest_path="../submissions/$cname/${uname}_${pname}.${ext}" #pATH
+dest_path="submissions/$cname/${uname}_${pname}.${ext}" #pATH
 
 cp "$spath" "$dest_path"
 
 echo "✅ Submitted "
 echo "Judging..."
-bash judge.sh "$dest_path" "$pname" "$uname" "$cname" #loc
+bash scripts/judge.sh "$dest_path" "$pname" "$uname" "$cname" #loc
