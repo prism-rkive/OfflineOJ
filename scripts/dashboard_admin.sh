5ace8fc4c4ebdf51f68819a4f4b4513d8eaa1249
Ayesha <u2104008@student.cuet.ac.bd>
2025-07-22 23:36:45 +0600

    Update dashboard_admin.sh

--- scripts/dashboard_admin.sh
+++ scripts/dashboard_admin.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
+
 while true; do
   echo "🛠️ Admin Dashboard"
   echo "1. Add Contest"
-  echo "2. View Users"
-  echo "3. Logout"
+  echo "2. View Problems" 
+  echo "3. Read Problem Statement"
+  echo "4. View Users"
+  echo "5. Logout"
   read -p "Select an option: " opt
 
-  if [[ "$opt" == "1" ]]; then
-    bash scripts/add_contest.sh
-  elif [[ "$opt" == "2" ]]; then
-    cat users.txt
-  elif [[ "$opt" == "3" ]]; then
-    echo "Logging out..."
-    exit
-  else
-    echo "Invalid option."
-  fi
+  case "$opt" in
+    1)
+      bash create_contest.sh 
+      ;;
+    2)
+
+        {
+            echo "Problem ID, Title, Difficulty, Status"
+            awk -F',' 'tolower($4) ~ /available/' ../problems.txt
+            awk -F',' 'tolower($4) !~ /available/' ../problems.txt
+        } | column -s, -t
+        ;;
+    3) 
+        read -p "📄 Enter Problem ID : " pid
+
+
+        echo "🔍$pid:"
+        echo "-----------------------------------"
+        cat "../problems/$pid/statement.txt"
+        echo "-----------------------------------"
+        ;;
+    4)
+        echo "👥 Users:"
+        {
+          echo "Username, Role"
+          awk -F',' '{gsub(/^ +| +$/, "", $0); print $1 "," $3}' ../users.txt
+        } | column -s, -t
+      
+       ;;
+
+    5)
+      echo "Logging out..."
+      exit
+      ;;
+    *)
+      echo "❌ Invalid option. Please try again."
+      ;;
+  esac
 done
